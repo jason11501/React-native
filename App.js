dbc56d794a6eca6826a783dbe28dbb370d9d0e22
jason11501 <55671823+jason11501@users.noreply.github.com>
2022-06-06 09:32:05 +0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{color: 'black', fontSize: 10, fontWeight: 'bold'}}>
+        Hello Jasonn!
+        </Text>
+      <Text>
+      Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
